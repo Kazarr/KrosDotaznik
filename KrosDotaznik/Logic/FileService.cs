998a7ebed51387a8546eff410a8146bc4ddb52c2
Kazar <kardos@kros.sk>
2019-06-14 15:12:41 +0200

    Method for serialization and deserialization

---
+++ KrosDotaznik/Logic/FileService.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class FileService
+    {
+        private void SaveJson(object objectToSerialize, string filePath)
+        {
+            string json = JsonConvert.SerializeObject(
+                        objectToSerialize,
+                        Formatting.Indented,
+                        new JsonSerializerSettings()
+                        {
+                            ContractResolver = new
+                            Newtonsoft.Json.Serialization
+                            .CamelCasePropertyNamesContractResolver()
+                        }
+                        );
+            using (TextWriter writer = new StreamWriter(filePath))
+            {
+                writer.Write(json);
+            }
+        }
+        private T LoadJson<T>(string path)
+        {
+            string json = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
