539003ff8c59013a7a40a94447fb6a3c53828363
JakubCelovsky <celovsky.jakubmail.com>
2019-06-14 13:20:27 +0200

    slovak resoureces change language fill questionare - just form

---
+++ KrosDotaznik/KrosDotaznik/FrmQuestionnaireToFill.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace KrosDotaznik
+{
+    partial class FrmQuestionnaireToFill
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grBoxEmployeeData = new System.Windows.Forms.GroupBox();
+            this.lblBirthDate = new System.Windows.Forms.Label();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.lblPreviousName = new System.Windows.Forms.Label();
+            this.lblMaidenName = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblIdentityCard = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.grBoxEmployeeData.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grBoxEmployeeData
+            // 
+            this.grBoxEmployeeData.Controls.Add(this.lblBirthDate);
+            this.grBoxEmployeeData.Controls.Add(this.lblSurname);
+            this.grBoxEmployeeData.Controls.Add(this.lblPreviousName);
+            this.grBoxEmployeeData.Controls.Add(this.lblMaidenName);
+            this.grBoxEmployeeData.Controls.Add(this.lblTitle);
+            this.grBoxEmployeeData.Controls.Add(this.lblIdentityCard);
+            this.grBoxEmployeeData.Controls.Add(this.lblName);
+            this.grBoxEmployeeData.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.grBoxEmployeeData.Location = new System.Drawing.Point(12, 44);
+            this.grBoxEmployeeData.Name = "grBoxEmployeeData";
+            this.grBoxEmployeeData.Size = new System.Drawing.Size(960, 221);
+            this.grBoxEmployeeData.TabIndex = 0;
+            this.grBoxEmployeeData.TabStop = false;
+            this.grBoxEmployeeData.Text = "groupBox1";
+            // 
+            // lblBirthDate
+            // 
+            this.lblBirthDate.AutoSize = true;
+            this.lblBirthDate.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblBirthDate.Location = new System.Drawing.Point(6, 175);
+            this.lblBirthDate.Name = "lblBirthDate";
+            this.lblBirthDate.Size = new System.Drawing.Size(57, 21);
+            this.lblBirthDate.TabIndex = 6;
+            this.lblBirthDate.Text = "label7";
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSurname.Location = new System.Drawing.Point(6, 70);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(57, 21);
+            this.lblSurname.TabIndex = 5;
+            this.lblSurname.Text = "label6";
+            // 
+            // lblPreviousName
+            // 
+            this.lblPreviousName.AutoSize = true;
+            this.lblPreviousName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblPreviousName.Location = new System.Drawing.Point(6, 91);
+            this.lblPreviousName.Name = "lblPreviousName";
+            this.lblPreviousName.Size = new System.Drawing.Size(57, 21);
+            this.lblPreviousName.TabIndex = 4;
+            this.lblPreviousName.Text = "label5";
+            // 
+            // lblMaidenName
+            // 
+            this.lblMaidenName.AutoSize = true;
+            this.lblMaidenName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblMaidenName.Location = new System.Drawing.Point(6, 112);
+            this.lblMaidenName.Name = "lblMaidenName";
+            this.lblMaidenName.Size = new System.Drawing.Size(57, 21);
+            this.lblMaidenName.TabIndex = 3;
+            this.lblMaidenName.Text = "label4";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTitle.Location = new System.Drawing.Point(6, 133);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(57, 21);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "label3";
+            // 
+            // lblIdentityCard
+            // 
+            this.lblIdentityCard.AutoSize = true;
+            this.lblIdentityCard.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblIdentityCard.Location = new System.Drawing.Point(6, 154);
+            this.lblIdentityCard.Name = "lblIdentityCard";
+            this.lblIdentityCard.Size = new System.Drawing.Size(57, 21);
+            this.lblIdentityCard.TabIndex = 1;
+            this.lblIdentityCard.Text = "label2";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblName.Location = new System.Drawing.Point(6, 49);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(57, 21);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "label1";
+            // 
+            // FrmQuestionnaireToFill
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 522);
+            this.Controls.Add(this.grBoxEmployeeData);
+            this.Name = "FrmQuestionnaireToFill";
+            this.Text = "FrmQuestionnaireToFill";
+            this.grBoxEmployeeData.ResumeLayout(false);
+            this.grBoxEmployeeData.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grBoxEmployeeData;
+        private System.Windows.Forms.Label lblBirthDate;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.Label lblPreviousName;
+        private System.Windows.Forms.Label lblMaidenName;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblIdentityCard;
+        private System.Windows.Forms.Label lblName;
+    }
+}
---
+++ KrosDotaznik/KrosDotaznik/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrosDotaznik
+{
+    public class MainViewModel
+    {
+        public void OpenQuestionnaireToFill()
+        {
+            using (FrmQuestionnaireToFill toFill = new FrmQuestionnaireToFill())
+            {
+                toFill.ShowDialog();
+            }
+        }
+
+        public void SetLOcalization(string language)
+        {
+            System.Globalization.CultureInfo.CurrentCulture = new System.Globalization.CultureInfo(language);
+            System.Globalization.CultureInfo.CurrentUICulture = new System.Globalization.CultureInfo(language);
+        }
+    }
+}
