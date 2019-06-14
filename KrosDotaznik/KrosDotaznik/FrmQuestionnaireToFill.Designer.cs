c2e5431a863064f0780c539686e71198981c14c7
JakubCelovsky <celovsky.jakubmail.com>
2019-06-14 13:50:19 +0200

    group EmployeeData - 40%

--- KrosDotaznik/KrosDotaznik/FrmQuestionnaireToFill.Designer.cs
+++ KrosDotaznik/KrosDotaznik/FrmQuestionnaireToFill.Designer.cs
@@ -36,11 +36,48 @@
             this.lblTitle = new System.Windows.Forms.Label();
             this.lblIdentityCard = new System.Windows.Forms.Label();
             this.lblName = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblPersonalId = new System.Windows.Forms.Label();
+            this.lblPlaceOfBirth = new System.Windows.Forms.Label();
+            this.lblRegionOfBirth = new System.Windows.Forms.Label();
+            this.lblNationality = new System.Windows.Forms.Label();
+            this.lblCitizenShip = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.lblPensionRecipient = new System.Windows.Forms.Label();
+            this.lblHandicapInPercentage = new System.Windows.Forms.Label();
+            this.lblHandicap = new System.Windows.Forms.Label();
+            this.lblSecondPillar = new System.Windows.Forms.Label();
+            this.lblHealthInsurance = new System.Windows.Forms.Label();
+            this.lblIBAN = new System.Windows.Forms.Label();
+            this.lblBankAcc = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblException = new System.Windows.Forms.Label();
             this.grBoxEmployeeData.SuspendLayout();
             this.SuspendLayout();
             // 
             // grBoxEmployeeData
             // 
+            this.grBoxEmployeeData.Controls.Add(this.lblException);
+            this.grBoxEmployeeData.Controls.Add(this.label4);
+            this.grBoxEmployeeData.Controls.Add(this.label3);
+            this.grBoxEmployeeData.Controls.Add(this.label2);
+            this.grBoxEmployeeData.Controls.Add(this.lblPensionRecipient);
+            this.grBoxEmployeeData.Controls.Add(this.lblHandicapInPercentage);
+            this.grBoxEmployeeData.Controls.Add(this.lblHandicap);
+            this.grBoxEmployeeData.Controls.Add(this.lblSecondPillar);
+            this.grBoxEmployeeData.Controls.Add(this.lblHealthInsurance);
+            this.grBoxEmployeeData.Controls.Add(this.lblIBAN);
+            this.grBoxEmployeeData.Controls.Add(this.lblBankAcc);
+            this.grBoxEmployeeData.Controls.Add(this.lblState);
+            this.grBoxEmployeeData.Controls.Add(this.lblGender);
+            this.grBoxEmployeeData.Controls.Add(this.lblCitizenShip);
+            this.grBoxEmployeeData.Controls.Add(this.lblNationality);
+            this.grBoxEmployeeData.Controls.Add(this.lblRegionOfBirth);
+            this.grBoxEmployeeData.Controls.Add(this.lblPlaceOfBirth);
+            this.grBoxEmployeeData.Controls.Add(this.lblPersonalId);
             this.grBoxEmployeeData.Controls.Add(this.lblBirthDate);
             this.grBoxEmployeeData.Controls.Add(this.lblSurname);
             this.grBoxEmployeeData.Controls.Add(this.lblPreviousName);
@@ -126,11 +163,201 @@
             this.lblName.TabIndex = 0;
             this.lblName.Text = "label1";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 272);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // lblPersonalId
+            // 
+            this.lblPersonalId.AutoSize = true;
+            this.lblPersonalId.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblPersonalId.Location = new System.Drawing.Point(201, 49);
+            this.lblPersonalId.Name = "lblPersonalId";
+            this.lblPersonalId.Size = new System.Drawing.Size(57, 21);
+            this.lblPersonalId.TabIndex = 7;
+            this.lblPersonalId.Text = "label7";
+            // 
+            // lblPlaceOfBirth
+            // 
+            this.lblPlaceOfBirth.AutoSize = true;
+            this.lblPlaceOfBirth.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblPlaceOfBirth.Location = new System.Drawing.Point(201, 70);
+            this.lblPlaceOfBirth.Name = "lblPlaceOfBirth";
+            this.lblPlaceOfBirth.Size = new System.Drawing.Size(57, 21);
+            this.lblPlaceOfBirth.TabIndex = 8;
+            this.lblPlaceOfBirth.Text = "label7";
+            // 
+            // lblRegionOfBirth
+            // 
+            this.lblRegionOfBirth.AutoSize = true;
+            this.lblRegionOfBirth.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblRegionOfBirth.Location = new System.Drawing.Point(201, 91);
+            this.lblRegionOfBirth.Name = "lblRegionOfBirth";
+            this.lblRegionOfBirth.Size = new System.Drawing.Size(57, 21);
+            this.lblRegionOfBirth.TabIndex = 9;
+            this.lblRegionOfBirth.Text = "label7";
+            // 
+            // lblNationality
+            // 
+            this.lblNationality.AutoSize = true;
+            this.lblNationality.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblNationality.Location = new System.Drawing.Point(201, 112);
+            this.lblNationality.Name = "lblNationality";
+            this.lblNationality.Size = new System.Drawing.Size(57, 21);
+            this.lblNationality.TabIndex = 10;
+            this.lblNationality.Text = "label7";
+            // 
+            // lblCitizenShip
+            // 
+            this.lblCitizenShip.AutoSize = true;
+            this.lblCitizenShip.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblCitizenShip.Location = new System.Drawing.Point(201, 133);
+            this.lblCitizenShip.Name = "lblCitizenShip";
+            this.lblCitizenShip.Size = new System.Drawing.Size(57, 21);
+            this.lblCitizenShip.TabIndex = 11;
+            this.lblCitizenShip.Text = "label7";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblGender.Location = new System.Drawing.Point(201, 154);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(57, 21);
+            this.lblGender.TabIndex = 12;
+            this.lblGender.Text = "label7";
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblState.Location = new System.Drawing.Point(201, 175);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(57, 21);
+            this.lblState.TabIndex = 13;
+            this.lblState.Text = "label7";
+            // 
+            // lblPensionRecipient
+            // 
+            this.lblPensionRecipient.AutoSize = true;
+            this.lblPensionRecipient.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblPensionRecipient.Location = new System.Drawing.Point(426, 175);
+            this.lblPensionRecipient.Name = "lblPensionRecipient";
+            this.lblPensionRecipient.Size = new System.Drawing.Size(57, 21);
+            this.lblPensionRecipient.TabIndex = 20;
+            this.lblPensionRecipient.Text = "label7";
+            // 
+            // lblHandicapInPercentage
+            // 
+            this.lblHandicapInPercentage.AutoSize = true;
+            this.lblHandicapInPercentage.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblHandicapInPercentage.Location = new System.Drawing.Point(426, 154);
+            this.lblHandicapInPercentage.Name = "lblHandicapInPercentage";
+            this.lblHandicapInPercentage.Size = new System.Drawing.Size(57, 21);
+            this.lblHandicapInPercentage.TabIndex = 19;
+            this.lblHandicapInPercentage.Text = "label7";
+            // 
+            // lblHandicap
+            // 
+            this.lblHandicap.AutoSize = true;
+            this.lblHandicap.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblHandicap.Location = new System.Drawing.Point(426, 133);
+            this.lblHandicap.Name = "lblHandicap";
+            this.lblHandicap.Size = new System.Drawing.Size(57, 21);
+            this.lblHandicap.TabIndex = 18;
+            this.lblHandicap.Text = "label7";
+            // 
+            // lblSecondPillar
+            // 
+            this.lblSecondPillar.AutoSize = true;
+            this.lblSecondPillar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSecondPillar.Location = new System.Drawing.Point(426, 112);
+            this.lblSecondPillar.Name = "lblSecondPillar";
+            this.lblSecondPillar.Size = new System.Drawing.Size(57, 21);
+            this.lblSecondPillar.TabIndex = 17;
+            this.lblSecondPillar.Text = "label7";
+            // 
+            // lblHealthInsurance
+            // 
+            this.lblHealthInsurance.AutoSize = true;
+            this.lblHealthInsurance.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblHealthInsurance.Location = new System.Drawing.Point(426, 91);
+            this.lblHealthInsurance.Name = "lblHealthInsurance";
+            this.lblHealthInsurance.Size = new System.Drawing.Size(57, 21);
+            this.lblHealthInsurance.TabIndex = 16;
+            this.lblHealthInsurance.Text = "label7";
+            // 
+            // lblIBAN
+            // 
+            this.lblIBAN.AutoSize = true;
+            this.lblIBAN.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblIBAN.Location = new System.Drawing.Point(426, 70);
+            this.lblIBAN.Name = "lblIBAN";
+            this.lblIBAN.Size = new System.Drawing.Size(57, 21);
+            this.lblIBAN.TabIndex = 15;
+            this.lblIBAN.Text = "label7";
+            // 
+            // lblBankAcc
+            // 
+            this.lblBankAcc.AutoSize = true;
+            this.lblBankAcc.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblBankAcc.Location = new System.Drawing.Point(426, 49);
+            this.lblBankAcc.Name = "lblBankAcc";
+            this.lblBankAcc.Size = new System.Drawing.Size(57, 21);
+            this.lblBankAcc.TabIndex = 14;
+            this.lblBankAcc.Text = "label7";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(582, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 21);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "label7";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(582, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 21);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "label7";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(582, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 21);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "label7";
+            // 
+            // lblException
+            // 
+            this.lblException.AutoSize = true;
+            this.lblException.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblException.Location = new System.Drawing.Point(582, 112);
+            this.lblException.Name = "lblException";
+            this.lblException.Size = new System.Drawing.Size(57, 21);
+            this.lblException.TabIndex = 24;
+            this.lblException.Text = "label7";
+            // 
             // FrmQuestionnaireToFill
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 522);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.grBoxEmployeeData);
             this.Name = "FrmQuestionnaireToFill";
             this.Text = "FrmQuestionnaireToFill";
@@ -150,5 +377,24 @@
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.Label lblIdentityCard;
         private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblPersonalId;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Label lblCitizenShip;
+        private System.Windows.Forms.Label lblNationality;
+        private System.Windows.Forms.Label lblRegionOfBirth;
+        private System.Windows.Forms.Label lblPlaceOfBirth;
+        private System.Windows.Forms.Label lblPensionRecipient;
+        private System.Windows.Forms.Label lblHandicapInPercentage;
+        private System.Windows.Forms.Label lblHandicap;
+        private System.Windows.Forms.Label lblSecondPillar;
+        private System.Windows.Forms.Label lblHealthInsurance;
+        private System.Windows.Forms.Label lblIBAN;
+        private System.Windows.Forms.Label lblBankAcc;
+        private System.Windows.Forms.Label lblException;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
--- KrosDotaznik/KrosDotaznik/FrmQuestionnaireToFill.cs
+++ KrosDotaznik/KrosDotaznik/FrmQuestionnaireToFill.cs
@@ -16,6 +16,7 @@ namespace KrosDotaznik
         {
             InitializeComponent();
             SetComponentsByLanguage();
+            Jahoda();
         }
 
         private void SetComponentsByLanguage()
@@ -28,6 +29,26 @@ namespace KrosDotaznik
             lblIdentityCard.Text = Resources.Questionnaire.lblIdentityCard;
             lblBirthDate.Text = Resources.Questionnaire.lblBirthDate;
             lblTitle.Text = Resources.Questionnaire.lblTitle;
+            lblBankAcc.Text = Resources.Questionnaire.lblBankAcc;
+            lblIBAN.Text = Resources.Questionnaire.lblIBAN;
+            lblPersonalId.Text = Resources.Questionnaire.lblPersonalId;
+            lblPlaceOfBirth.Text = Resources.Questionnaire.lblPlaceOfBirth;
+            lblRegionOfBirth.Text = Resources.Questionnaire.lblRegionOfBirth;
+            lblNationality.Text = Resources.Questionnaire.lblNationality;
+            lblCitizenShip.Text = Resources.Questionnaire.lblCitizenShip;
+            lblGender.Text = Resources.Questionnaire.lblGender;
+            lblState.Text = Resources.Questionnaire.lblState;
+            lblHealthInsurance.Text = Resources.Questionnaire.lblHealthInsurance;
+            lblSecondPillar.Text = Resources.Questionnaire.lblSecondPillar;
+            lblHandicap.Text = Resources.Questionnaire.lblHandicap;
+            lblHandicapInPercentage.Text = Resources.Questionnaire.lblHandicapInPercentage;
+            lblPensionRecipient.Text = Resources.Questionnaire.lblPensionRecipient;
+            lblException.Text = Resources.Questionnaire.lblException;
+
+        }
+        private void Jahoda()
+        {
+            groupBox1.Visible = false;
         }
     }
 }
