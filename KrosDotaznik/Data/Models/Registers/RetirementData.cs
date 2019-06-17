c5f083251b7088b435adbe9ff96a6456a2a386b8
Kazar <kardos@kros.sk>
2019-06-17 09:37:44 +0200

    Comment on retirementInsuranceComapnyException in RetirementData

--- KrosDotaznik/Data/Models/Registers/RetirementData.cs
+++ KrosDotaznik/Data/Models/Registers/RetirementData.cs
@@ -12,6 +12,9 @@ namespace Data.Models.Registers
         public int Id { get; set; }
         public bool ParticipatingInRetirementSaving { get; set; }
         public DateTime? RetiredSince { get; set; }
+        /// <summary>
+        /// Only contranct work type should set this.
+        /// </summary>
         public bool? RetirementInsuranceCompanyException { get; set; }
         public Retirement Retirement { get; set; }
     }
