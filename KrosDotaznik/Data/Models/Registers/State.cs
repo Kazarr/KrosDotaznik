25b5800a141b2a55ce3fb8207e36577521865f47
Kazar <kardos@kros.sk>
2019-06-17 09:11:45 +0200

    Comment on employeestate

--- KrosDotaznik/Data/Models/Registers/State.cs
+++ KrosDotaznik/Data/Models/Registers/State.cs
@@ -9,6 +9,9 @@ namespace Data.Models.Registers
     public class State
     {
         public int Id { get; set; }
+        /// <summary>
+        /// Married, divorced, single...
+        /// </summary>
         public string EmployeeState { get; set; }
     }
 }
