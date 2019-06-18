866617b4192a0b690ff10b15de93f4444cd9541e
Miroslav Valjašek <miroslav.valjasek@gmail.com>
2019-06-18 13:49:43 +0200

    pridanie metodkz na nacitanie dat pre combobox

---
+++ KrosDotaznik/Data/ConnectionManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ConnectionManager
+    {
+        public DataTable GetAllAccessData(string command)
+        {
+            OleDbConnection connection = new OleDbConnection();
+            connection.ConnectionString = Properties.SettingsAccess.Default.ConnString;
+            connection.Open();
+            OleDbCommand dbCommand = new OleDbCommand();
+            dbCommand.CommandText = command;
+            dbCommand.Connection = connection;
+            OleDbDataAdapter oleDbData = new OleDbDataAdapter(dbCommand);
+            DataSet ds = new DataSet();
+            oleDbData.Fill(ds);
+            return ds.Tables[0];
+        }
+    }
+}
