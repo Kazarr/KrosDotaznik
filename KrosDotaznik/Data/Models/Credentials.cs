fd9a4799bbdc03e6d60435235d7e1862a32ee711
Kazar <kardos@kros.sk>
2019-06-20 10:52:03 +0200

    Crypitng with constant key and IV. Credentials no longer List<byte[]> only byte[]

--- KrosDotaznik/Data/Models/Credentials.cs
+++ KrosDotaznik/Data/Models/Credentials.cs
@@ -9,7 +9,7 @@ namespace Data.Models
     public class Credentials
     {
         public int Id { get; set; }
-        public List<byte[]> PaycheckPassword { get; set; }
-        public List<byte[]> PinAlarm { get; set; }
+        public byte[] PaycheckPassword { get; set; }
+        public byte[] PinAlarm { get; set; }
     }
 }
