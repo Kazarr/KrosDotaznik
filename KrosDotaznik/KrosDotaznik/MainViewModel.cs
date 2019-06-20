63d3febd220ce7b332214369a74f1849459229ae
Miroslav Valjašek <miroslav.valjasek@gmail.com>
2019-06-20 08:07:07 +0200

    Add configure mode for questionare

--- KrosDotaznik/KrosDotaznik/MainViewModel.cs
+++ KrosDotaznik/KrosDotaznik/MainViewModel.cs
@@ -5,11 +5,107 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Logic;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
 
 namespace KrosDotaznik
 {
     public class MainViewModel
     {
+        #region Fields
+        private bool[] _showQuestionGroups = new bool[7];
+        private bool _employeeData;
+        private bool _contactsAddress;
+        private bool _passwordAccess;
+        private bool _educationData;
+        private bool _previousJob;
+        private bool _childData;
+        private bool _jobSpecification;
+        #endregion
+
+        #region Properties
+        public bool EmployeeData
+        {
+            get => _employeeData;
+            set
+            {
+                _employeeData = value;
+                _showQuestionGroups[0] = _employeeData;
+                OnPropertyChange();
+            }
+        }
+        public bool ContactsAddress
+        {
+            get => _contactsAddress;
+            set
+            {
+                _contactsAddress = value;
+                _showQuestionGroups[1] = _contactsAddress;
+                OnPropertyChange();
+            }
+        }
+        public bool PasswordAccess
+        {
+            get => _passwordAccess;
+            set
+            {
+                _passwordAccess = value;
+                _showQuestionGroups[2] = _passwordAccess;
+                OnPropertyChange();
+            }
+        }
+        public bool EducationData
+        {
+            get => _educationData;
+            set
+            {
+                _educationData = value;
+                _showQuestionGroups[3] = _educationData;
+                OnPropertyChange();
+            }
+        }
+        public bool PreviousJob
+        {
+            get => _previousJob;
+            set
+            {
+                _previousJob = value;
+                _showQuestionGroups[4] = _previousJob;
+                OnPropertyChange();
+            }
+        }
+        public bool ChildData
+        {
+            get => _childData;
+            set
+            {
+                _childData= value;
+                _showQuestionGroups[5] = _childData;
+                OnPropertyChange();
+            }
+        }
+        public bool JobSpecification
+        {
+            get => _jobSpecification;
+            set
+            {
+                _jobSpecification = value;
+                _showQuestionGroups[6] = _jobSpecification;
+                OnPropertyChange();
+            }
+        }
+        #endregion
+
+        #region Inotify Property Change
+
+        private void OnPropertyChange([CallerMemberName] string propertyName = "")
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Form method
         public void OpenQuestionnaireToFill()
         {
             using (FrmQuestionnaireToFill toFill = new FrmQuestionnaireToFill(new QuestionnaireToFillViewModel()))
@@ -23,55 +119,18 @@ namespace KrosDotaznik
             System.Globalization.CultureInfo.CurrentCulture = new System.Globalization.CultureInfo(language);
             System.Globalization.CultureInfo.CurrentUICulture = new System.Globalization.CultureInfo(language);
         }
-        /// <summary>
-        /// Testing method to test correctness and see output json
-        /// </summary>
-        public void SaveTest()
+        #endregion
+
+        #region Save method
+        public void SaveQuestionnaire()
         {
             FileService fs = new FileService();
             fs.SaveJson(
                 new Questionare()
                 {
-                    ShowQuestionGroups = new bool[10],
-                    Employee = new Employee()
-                    {
-                        Name = "Miro",
-                        Surname = "Valo",
-                        Title = "Ing.",
-                        BirthDate = new DateTime(1992, 11, 26),
-                        Gender = true,
-                        BankAccountNumber = "123456798",
-                        State = new Data.Models.Registers.State() { Id = 1, EmployeeState = "Married" },
-                        HealthInsuranceCompany = new Data.Models.Registers.HealthInsurance() { CompanyName = "Generali" },
-                        AddressData = new Address() { City = "Zilina", PostalCode = 01001, Street = "Varin", HouseNumber = "50/5" },
-                        Credentials = new Credentials() { PaycheckPassword = fs.Encrypt("nbu123"), PinAlarm = fs.Encrypt("123456") },
-                        HighestEducationData = new Education() { EducationLevel = new Data.Models.Registers.EducationLevel() { EducationLevelName = "Bachelor" }, EndYear = new DateTime(2000, 10, 15), Major = "Informatics", School = "Fri" },
-                        PreviousJobData = new PreviousJob() { Position = "Upratovac", EmployerCompanyName = "Upratovacia Cata", StartDate = new DateTime(1995, 5, 3), EndDate = new DateTime(2015, 8, 12) },
-                        JobSpecificationData = new JobSpecification()
-                        {
-                            HireDate = new DateTime(2018, 1, 1),
-                            StartDate = new DateTime(2018, 1, 2),
-                            EndOfTrial = new DateTime(2018, 3, 1),
-                            Department = new Data.Models.Registers.Department() { DepartmentName = "4.1", ParentId = 4 },
-                            WorkExpiration = new Data.Models.Registers.WorkExpiration() { JobExpire = "Unlimited" },
-                            WorkHours = new Data.Models.Registers.WorkHours() { NameOfHoursPerWeek = "FullTime" },
-                            WorkType = new Data.Models.Registers.WorkType() { Type = "Employment" },
-                            WorkPosition = new Data.Models.Registers.WorkPosition() { Position = "programator", WageCategory = new Data.Models.Registers.WageCategory() { Category = "A" } },
-                            WorkPlace = "Zilina",
-
-                        },
-                        Children = new List<Child>() { new Child() { Name = "Jozko", Surname = "Mrkvicka", Bonus = true, BirthDate = new DateTime(2000, 4, 1) }, new Child() { Name = "Ferko", Surname = "Mrkvicka", Bonus = true, BirthDate = new DateTime(2005, 7, 5) } }
-                    }
+                    ShowQuestionGroups = _showQuestionGroups, 
                 }, "test.json");
         }
-        /// <summary>
-        /// Testing method. Testing correctness.
-        /// </summary>
-        /// <returns></returns>
-        public Questionare LoadTest()
-        {
-            FileService fs = new FileService();
-            return fs.LoadJson<Questionare>("test.json");
-        }
+        #endregion
     }
 }
